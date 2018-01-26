83e34268cb7e33cb8eeae6359ca6c63a9281c934
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-01-26 18:35:08 +0100

    Corregido Ejemplo01

--- Ejemplo01.m
+++ Ejemplo01.m
@@ -18,8 +18,8 @@ ua = 0; ub = 1; % Contorno
 [x3, u3] = bvp(a, b, N, mu, V3, sigma, f, ua, ub)
 
 sol1 = inline('(exp(V1 * x) - 1) ./ (exp(V1) - 1)', 'x')
-sol1 = inline('(exp(V2 * x) - 1) ./ (exp(V2) - 1)', 'x')
-sol1 = inline('(exp(V3 * x) - 1) ./ (exp(V3) - 1)', 'x')
+sol2 = inline('(exp(V2 * x) - 1) ./ (exp(V2) - 1)', 'x')
+sol3 = inline('(exp(V3 * x) - 1) ./ (exp(V3) - 1)', 'x')
 
 
 subplot(1, 3, 1)
@@ -28,11 +28,11 @@ legend('aprox', 'exacta');
 title('V = 1');
 
 subplot(1, 3, 2)
-plot(x2, u2, x2, sol1(x2))
+plot(x2, u2, x2, sol2(x2))
 legend('aprox', 'exacta');
 title('V = 25');
 
 subplot(1, 3, 3)
-plot(x3, u3, x3, sol1(x3))
+plot(x3, u3, x3, sol3(x3))
 legend('aprox', 'exacta');
 title('V = 100');
